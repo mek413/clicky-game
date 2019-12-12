b627596a55eb9db2d29deb044d05f94423c9c706
Marcus Kelly <mek413@gmail.com>
2019-12-12 17:26:34 -0600

    added basic starting files in src

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import cards from "./cards.json";
+
+class App extends Component {
+    // Setting this.state.friends to the friends json array
+    state = {
+      friends
+    };
+  
+    // Map over this.state.friends and render a FriendCard component for each friend object
+    render() {
+      return (
+        <h1>Hello World</h1>
+      )
+    }
+  }
+  
+  export default App;  
