224ef1e99d0e308e5396f6089ad6cf8eb0499066
Marcus Kelly <mek413@gmail.com>
2019-12-14 15:30:51 -0600

    Updates

---
+++ precache-manifest.f9f35bf79b4b5b18eff9378b81292cbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4be566b68d26ee42db7d2aca94cfe6b4",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "13799e53de119a75a62d",
+    "url": "/clicky-game/static/css/main.8006ca79.chunk.css"
+  },
+  {
+    "revision": "00d61d0bbab4af0d0cec",
+    "url": "/clicky-game/static/js/2.59c7650b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.59c7650b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "13799e53de119a75a62d",
+    "url": "/clicky-game/static/js/main.52eaeca7.chunk.js"
+  },
+  {
+    "revision": "b859e8c835d6a54c0ef8",
+    "url": "/clicky-game/static/js/runtime-main.bf081868.js"
+  }
+]);
